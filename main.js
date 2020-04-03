19d23a1363967679a5f57f396fc0a2882e55b324
stara1991 <dmitriy.starunskiy@gmail.com>
2020-04-03 13:56:26 +0200

    fixed bugs

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
       userService.getAll().then(users => {
         resolve(users);
       });
-    }, 2000);
+    }, 1000);
   });
 
   const users = await usersPromise;
@@ -23,8 +23,84 @@
 
     const candidateRowElement = document.createElement("div");
     candidateRowElement.addEventListener("click", function() {
+      const candidateId = candidate.id;
       console.log("click");
       tableCandidats.style.display = "none";
+
+      var changeCandidateFormElem = document.createElement("FORM");
+      changeCandidateFormElem.setAttribute("id", "myForm");
+      document.body.appendChild(changeCandidateFormElem);
+
+      var firstNameInputElem = document.createElement("INPUT");
+      firstNameInputElem.setAttribute("type", "text");
+      firstNameInputElem.setAttribute("value", candidate.first_name);
+      changeCandidateFormElem.appendChild(firstNameInputElem);
+
+      var lastNameInputElem = document.createElement("INPUT");
+      lastNameInputElem.setAttribute("type", "text");
+      lastNameInputElem.setAttribute("value", candidate.last_name);
+      changeCandidateFormElem.appendChild(lastNameInputElem);
+
+      var emailInputElem = document.createElement("INPUT");
+      emailInputElem.setAttribute("type", "text");
+      emailInputElem.setAttribute("value", candidate.email);
+      changeCandidateFormElem.appendChild(emailInputElem);
+
+      var dobInputElem = document.createElement("INPUT");
+      dobInputElem.setAttribute("type", "text");
+      dobInputElem.setAttribute("value", candidate.dob);
+      changeCandidateFormElem.appendChild(dobInputElem);
+
+      var changeCandidateButton = document.createElement("INPUT");
+      changeCandidateButton.setAttribute("type", "submit");
+      changeCandidateButton.setAttribute("value", "CHANGE");
+      changeCandidateFormElem.appendChild(changeCandidateButton);
+
+      changeCandidateButton.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        const data = {
+          first_name: firstNameInputElem.value,
+          last_name: lastNameInputElem.value,
+          email: emailInputElem.value,
+          dob: dobInputElem.value
+        };
+
+        function hideErrors() {
+          debugger;
+          if (errorMessages.length) {
+            for (var i = 0; i < errorMessages.length; i++) {
+              const elem = errorMessages[i];
+              console.log(elem);
+              elem.style.display = "none";
+            }
+          }
+        }
+
+        const errorMessages = document.getElementsByClassName("error-message");
+        userService.updateUser(candidateId, data).then(AAAA => {
+          if (Array.isArray(AAAA) && AAAA[0].message) {
+            debugger;
+            console.log("HAVE ERRORS");
+            hideErrors();
+
+            AAAA.forEach(item => {
+              debugger;
+              const errorMessageElem = document.createElement("span");
+              errorMessageElem.style.color = "red";
+              errorMessageElem.classList.add("error-message");
+              errorMessageElem.innerHTML = item.message;
+              changeCandidateFormElem.appendChild(errorMessageElem);
+            });
+            /// ploho
+          } else {
+            debugger;
+            console.log("DONT HAVE ERRORS");
+            hideErrors();
+            // horosho
+          }
+        });
+      });
     });
 
     candidateRowElement.classList.add("row");
--- services/user-service.js
+++ services/user-service.js
@@ -1,25 +1,42 @@
-const userService = (function () {
+const userService = (function() {
+  function getAllUsers() {
+    var opts = {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer _WprxQEHuNUhg9ymuxGvP9ZLvHv434Dtqokp"
+      }
+    };
 
-    function getAllUsers() {
+    return fetch("https://gorest.co.in/public-api/users", opts)
+      .then(response => response.json())
+      .then(response => response.result);
+  }
 
-        var opts = {
-            method: 'GET',
-            mode: 'cors',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer _WprxQEHuNUhg9ymuxGvP9ZLvHv434Dtqokp'
-            }
-        };
+  function updateUser(userId, updatedUser) {
+    var opts = {
+      method: "PUT",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer _WprxQEHuNUhg9ymuxGvP9ZLvHv434Dtqokp"
+      },
+      body: JSON.stringify(updatedUser)
+    };
 
-        return fetch('https://gorest.co.in/public-api/users', opts).then(response => response.json()).then(response => response.result);
-    }
+    return fetch("https://gorest.co.in/public-api/users/" + userId, opts)
+      .then(response => response.json())
+      .then(response => response.result);
+  }
 
-    return {
-        getAll: getAllUsers
-    }
-})()
-
+  return {
+    getAll: getAllUsers,
+    updateUser //если название и имя свойства одно и тоже -пишем только одно
+  };
+})();
 
 // const userService = {
 //     get: function () {
